efec4ab19ae86556f3cb80d964b2642a3db1d4d5
Safayet-Shahriar <sssumve@gmail.com>
2022-03-26 19:28:10 +0600

    cmt

---
+++ data.js
@@ -0,0 +1,14 @@
+let names = [];
+let times = [];
+let inputname;
+const d = new Date();
+let text = d.toString();
+
+function check() {
+    do {
+      inputname = String(prompt("Enter your name to be confirmed."))
+    }
+    while (inputname == "" || inputname == " ");
+    names.push(inputname);
+    times.push(text);
+}
